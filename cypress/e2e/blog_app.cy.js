7307f5e144183c6b7b34050fc65b5177ab36cf33
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-03-12 18:40:23 +0200

    5.18: Blog List End To End Testing, step 2

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,10 +1,48 @@
 describe('Blog app', function() {
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'root',
+      username: 'Superuser',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+
+    it('succeeds with correct credentials', function() {
+      cy.request('POST', 'http://localhost:3003/api/login', {
+        username: 'Superuser', password: 'salainen'
+      }).then(response => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:5173')
+        cy.contains('root logged in')
+      })
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3003/api/login',
+        failOnStatusCode: false,
+        body: {
+          username: 'nonExistingUser',
+          password: 'salainen'
+        }
+      }).then(response => {
+        localStorage.setItem('loggedBlogappUser', JSON.stringify(response.body))
+        cy.visit('http://localhost:5173')
+        expect(response.status).to.eq(401)
+        cy.contains('root logged in').should('not.exist')
+      })
+    })
+
   })
 })
